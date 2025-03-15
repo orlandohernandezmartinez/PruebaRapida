35ad8f0f7f19b30b56387ce8362e66115b899fa1
orlandohernandezmartinez <orlando@vacacapital.com>
2025-03-14 19:48:21 -0600

    Prueba simple

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from minimal Flask!"
+
+@app.route("/ping")
+def ping():
+    return "pong"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
